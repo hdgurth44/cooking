9636b5eb3ad3c87e92b49f7f860f9b81fbee4a51
Harry Daniel Gurth Angeles <hdgurth@gmail.com>
2025-09-24 11:09:55 +0200

    Make sure to ignore the.gitignore file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const PORT = ENV.PORT;
@@ -37,6 +38,34 @@ app.post("/api/favorites", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// Delete a favorite
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+    try {
+        const { userId, recipeId } = req.params
+        await db.delete(favoritesTable).where(
+            and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, recipeId))
+        )
+        res.status(200).json({ message: "Favorite removed successfully" });
+    } catch (error) {
+        console.error("Error removing favorite:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
+
+// Get all favorites for a user
+
+app.get("/api/favorites/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params // get the userId from the request params
+        const favorites = await db.select().from(favoritesTable).where(eq(favoritesTable.userId, userId)) // assign the results of the query to the favorites variable
+        res.status(200).json(favorites) // send the favorites as a response
+    } catch (error) {
+        console.error("Error getting favorites:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 app.listen(PORT, () => {
   console.log("Server is running on port: ", PORT);
