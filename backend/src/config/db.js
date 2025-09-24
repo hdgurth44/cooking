99e60d9f9525d1da336ce2dd69e1f62e556e4948
Harry Daniel Gurth Angeles <hdgurth@gmail.com>
2025-09-24 20:25:00 +0200

    Delete old back-end

--- backend/src/config/db.js
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import { ENV } from "./env.js";
-import * as schema from "../db/schema.js";
-
-const sql = neon(ENV.DATABASE_URL);
-export const db = drizzle(sql, { schema });
