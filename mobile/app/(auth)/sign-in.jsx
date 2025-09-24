ac32f98276afc8159b0151b97ec94b8205fe6a31
Harry Daniel Gurth Angeles <hdgurth@gmail.com>
2025-09-24 15:28:45 +0200

    Refactor favorites API endpoint to improve JSON response handling

---
+++ mobile/app/(auth)/sign-in.jsx
@@ -0,0 +1,144 @@
+import {
+  View,
+  Text,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import { useSignIn } from "@clerk/clerk-expo";
+import { authStyles } from "../../assets/styles/auth.styles";
+import { Image } from "expo-image";
+import { COLORS } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+
+const SignInScreen = () => {
+  const router = useRouter();
+
+  const { signIn, setActive, isLoaded } = useSignIn();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // Whether we show the password or not, with false being the default.
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = async () => {
+    if (!email || !password) {
+      Alert.alert("Please fill in all fields");
+      return;
+    }
+    if (!isLoaded) return;
+
+    setLoading(true);
+    try {
+      const signInAttempt = await signIn.create({
+        emailAddress: email,
+        password,
+      });
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+      } else {
+        Alert.alert("Error", "Something went wrong");
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err) {
+      Alert.alert("Error", err.errors?.[0]?.message || "Something went wrong");
+      console.error(JSON.stringify(err, null, 2));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+        style={authStyles.keyboardView}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+      >
+        <ScrollView
+          style={authStyles.scrollContent}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={authStyles.imageContainer}>
+            <Image
+              source={require("../../assets/images/i1.png")}
+              style={authStyles.image}
+              contentFit="contain"
+            />
+          </View>
+          <Text style={authStyles.title}>Welcome Back</Text>
+          {/* Form container */}
+          <View style={authStyles.formContainer}>
+            {/* Email input */}
+            <View style={authStyles.inputContainer}>
+              <TextInput
+                style={authStyles.textInput}
+                placeholder="Enter your email"
+                placeholderTextColor={COLORS.textLight}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+            {/* Password input */}
+            <View style={authStyles.inputContainer}>
+              <TextInput
+                style={authStyles.textInput}
+                placeholder="Enter your password"
+                placeholderTextColor={COLORS.textLight}
+                value={password}
+                onChangeText={setPassword}
+                keyboardType="default"
+                autoCapitalize="none"
+                secureTextEntry={!showPassword}
+              />
+              <TouchableOpacity
+                style={authStyles.eyeButton}
+                onPress={() => setShowPassword(!showPassword)}
+              >
+                <Ionicons
+                  name={showPassword ? "eye-outline" : "eye-off-outline"}
+                  size={20}
+                  color={COLORS.textLight}
+                />
+              </TouchableOpacity>
+            </View>
+            {/* Sign in button */}
+            <TouchableOpacity
+              style={[
+                authStyles.authButton, // Always applies this style
+                loading && authStyles.buttonDisabled, // Applies disabled style if loading is true. Main use case for [].
+              ]}
+              disabled={loading}
+              onPress={handleSignIn}
+              activeOpacity={0.8}
+            >
+              <Text style={authStyles.buttonText}>
+                {loading ? "Signing in..." : "Sign In"}
+              </Text>
+            </TouchableOpacity>
+            {/* Sign up link */}
+            <TouchableOpacity
+              style={authStyles.linkContainer}
+              onPress={() => router.push("/(auth)/sign-up")}
+            >
+              <Text style={authStyles.linkText}>
+                Don&apos;t have an account?{" "}
+                <Text style={authStyles.link}>Sign Up</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+export default SignInScreen;
